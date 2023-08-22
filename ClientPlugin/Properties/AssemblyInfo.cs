c1a5ebc0307b19aa2f87fd911e20eca5e4dd271e
InvalidArgument3 <InvalidArgument3@gmail.com>
2023-08-22 17:30:19 -0500

    hello?

--- ClientPlugin/Properties/AssemblyInfo.cs
+++ ClientPlugin/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PluginTemplate")]
+[assembly: AssemblyTitle("SteamMaintLobotomizer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PluginTemplate")]
+[assembly: AssemblyProduct("SteamMaintLobotomizer")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("A061FC6C-713E-42CD-B413-151AC8A5074C")]
+[assembly: Guid("FD9C11EA-AC51-49BF-BB09-3174DB780DD2")]
 
 // Version information for an assembly consists of the following four values:
 //
